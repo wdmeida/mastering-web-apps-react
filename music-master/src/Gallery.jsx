83aa668309f9533295c36943a6f5749ae79ef06d
Wagner Almeida <wdmeida@gmail.com>
2017-12-19 09:52:17 -0200

    Customized icons to play/stop preview audio in top tracks

--- music-master/src/Gallery.jsx
+++ music-master/src/Gallery.jsx
@@ -3,9 +3,39 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playingUrl: '',
+      audio: null,
+      playing: false
+    }
+  }
+
   playAudio(previewUrl) {
     let audio = new Audio(previewUrl);
     audio.play();
+    if (!this.state.playing) {
+      audio.play();
+      this.setState({
+        playing: true,
+        playingUrl: previewUrl,
+        audio 
+      });
+    } else {
+      if (this.state.playingUrl === previewUrl) {
+        this.state.audio.pause();
+        this.setState({ playing: false });
+      } else {
+        this.state.audio.pause();
+        audio.play();
+        this.setState({
+          playing: true,
+          playingUrl: previewUrl,
+          audio
+        });
+      }
+    }
   }
 
   render() {
@@ -25,6 +55,16 @@ class Gallery extends Component {
                 className='track-img'
                 alt='Track'
               />
+              <div className="track-play">
+                <div className="track-play-inner">
+                 
+                  {
+                    this.state.playingUrl === track.preview_url
+                      ? <span>| |</span>
+                      : <span>&#9654;</span>
+                  }
+                </div>
+              </div>
               <p className="track-text">
                 {track.name}
               </p>

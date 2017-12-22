8e992e1c9a2436c1b64799a1260e5b6ae35ebb79
Wagner Almeida <wdmeida@gmail.com>
2017-12-22 15:47:39 -0200

    Adding components and configure redux

--- reminder-pro/src/index.js
+++ reminder-pro/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducers from './reducers';
+import App from './components/App';
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);

50d9addfb19f25243e5602e560e8931612e38510
Wagner Almeida <wdmeida@gmail.com>
2017-12-22 19:14:52 -0200

    Added react-bootstrap and index.css

--- reminder-pro/src/components/App.jsx
+++ reminder-pro/src/components/App.jsx
@@ -1,29 +1,73 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { addReminder } from '../actions';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  addReminder() {
+    this.props.addReminder(this.state.text);
+  }
+
+  renderReminders() {
+    const { reminders } = this.props;
+    return (
+      <ul className="list-group col-sm-4">
+        {
+          reminders.map(reminder => {
+            return (
+              <li key={reminder.id} className='list-group-item'>
+                <div>{reminder.text}</div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <div className="title">
           Reminder Pro
         </div>
-        <div className="form-inline">
+        <div className="form-inline reminder-form">
           <div className="form-group">
             <input 
               className="form-control"
               placeholder='I have to...'
+              onChange={event => this.setState({text: event.target.value})}
             />
-          </div>
+          </div> 
           <button
             type='button'
             className='btn btn-success'
+            onClick={() => this.addReminder()}
           >
             Add Reminder
           </button>
         </div>
+        { this.renderReminders() }
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  reminders: state
+})
+
+const mapDispatchToProps = (dispatch) => (
+  bindActionCreators({addReminder}, dispatch)
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- reminder-pro/src/index.js
+++ reminder-pro/src/index.js
@@ -6,6 +6,8 @@ import { createStore } from 'redux';
 import reducers from './reducers';
 import App from './components/App';
 
+import './index.css';
+
 const store = createStore(reducers);
 
 ReactDOM.render(
